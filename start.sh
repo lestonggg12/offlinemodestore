e7d37ef03925cd76f0310678d7b8095da6bf995a
lestonggg12 <leca.alcantara.coc@phinmaed.com>
2026-03-03 16:12:43 +0800

    fix: fake migration 0016 to bypass duplicate table

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 python manage.py collectstatic --noinput
-python manage.py migrate
+python manage.py migrate store 0016 --fake && python manage.py migrate
 gunicorn sarisaristore.sarisaristore.wsgi:application --bind 0.0.0.0:$PORT
